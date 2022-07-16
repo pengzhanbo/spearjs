d89c37e367e1d01158a2b1371dacf3b6f7752074
pengzhanbo <volodymyr@foxmail.com>
2022-07-16 14:27:13 +0800

    build: upgrade vite to 3.0.0

--- packages/editor/windi.config.ts
+++ packages/editor/windi.config.ts
@@ -4,6 +4,7 @@ import typography from 'windicss/plugin/typography'
 
 export default defineConfig({
   darkMode: 'class',
+  // attributify: true,
   plugins: [typography()],
   theme: {
     extend: {
