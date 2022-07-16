447a8e02630f867b82269d694be23549aa65f9b1
pengzhanbo <volodymyr@foxmail.com>
2022-07-16 11:14:14 +0800

    refactor(editor): 调整文件引入方式

--- packages/editor/src/components/Home/index.tsx
+++ packages/editor/src/components/Home/index.tsx
@@ -1,11 +1,11 @@
+import LeftSidebar from '@editor/components/LeftSidebar'
+import Navbar from '@editor/components/Navbar'
+import RightController from '@editor/components/RightController'
+import Stage from '@editor/components/Stage'
 import { useAppLayout, useContextMenu } from '@editor/hooks'
 import { useAppPagesStore } from '@editor/stores'
 import type { Ref } from 'vue'
 import { defineComponent, ref, withModifiers } from 'vue'
-import LeftSidebar from '../LeftSidebar'
-import Navbar from '../Navbar'
-import RightController from '../RightController'
-import Stage from '../Stage'
 import styles from './index.module.scss'
 
 export default defineComponent({
