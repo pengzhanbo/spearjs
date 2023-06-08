2a667ada017e144840ab08f2a64c12eeab369290
pengzhanbo <volodymyr@foxmail.com>
2023-06-09 00:33:01 +0800

    build: uppdate eslint & prettier & stylelint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,117 +1,12 @@
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-    jest: true,
-  },
-  parser: 'vue-eslint-parser',
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    jsxPragma: 'React',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  extends: [
-    'plugin:vue/vue3-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/recommended',
-    'plugin:prettier/recommended',
-  ],
-  rules: {
-    '@typescript-eslint/consistent-type-imports': 'warn',
-    'vue/script-setup-uses-vars': 'error',
-    'vue/component-tags-order': [
-      'error',
-      {
-        order: ['script', 'template', 'style'],
-      },
-    ],
-    'vue/multi-word-component-names': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-      },
-    ],
-    'no-unused-vars': [
-      'error',
-      {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-      },
-    ],
-    '@typescript-eslint/no-empty-function': 'off',
-    'import/no-cycle': 'off',
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: [
-          '**/__tests__/**',
-          '**/.eslintrc.js',
-          '**/jest.config.js',
-          '**/jest.setup.js',
-          '**/tsup.config.js',
-          '**/vitest.config.js',
-          '**/vite.config.ts',
-          '**/windi.config.ts',
-          '**/*.spec.ts',
-          'packages/server/test/*.ts',
-        ],
-        optionalDependencies: false,
-        peerDependencies: false,
-      },
-    ],
-    'import/order': [
-      'error',
-      {
-        'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-        'newlines-between': 'never',
-        'alphabetize': {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-      },
-    ],
-    'sort-imports': [
-      'warn',
-      {
-        ignoreCase: true,
-        ignoreDeclarationSort: true,
-        ignoreMemberSort: false,
-        allowSeparatedGroups: false,
-      },
-    ],
-    'import/no-unresolved': 'off',
-    'import/prefer-default-export': 'off',
-    'import/named': 'off',
-  },
+  extends: ['@pengzhanbo/eslint-config-vue'],
   overrides: [
     {
-      files: ['*.vue'],
-      // @ts-ignore
-      globals: {
-        defineEmits: 'readonly',
-        defineProps: 'readonly',
-      },
-      rules: {
-        // disable for setup script
-        '@typescript-eslint/no-unused-vars': 'off',
-      },
-    },
-    {
-      files: ['./packages/server/src/**/*.ts', './packages/server/test/**/*.ts'],
+      files: [
+        './packages/server/src/**/*.ts',
+        './packages/server/test/**/*.ts',
+      ],
       rules: {
         '@typescript-eslint/consistent-type-imports': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
